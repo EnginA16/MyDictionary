0d69c9a9896890038b458c77a534c24b7529d92b
Engin Aydın <engin1637e@gmail.com>
2021-01-21 16:49:14 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+            isimler.Add("Halil");
+            isimler.Add("Engin");
+            isimler.Add("Burak");
+            isimler.Add("Talha");
+        }
+    }
+}
